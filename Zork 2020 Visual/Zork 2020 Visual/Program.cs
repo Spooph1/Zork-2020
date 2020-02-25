27b2f70a6dc50b4c9122a150a2cc050b15ff79be
Simon Juul <simon@documentary.dk>
2020-02-25 14:05:05 +0100

    Lavet projektet

---
+++ Zork 2020 Visual/Zork 2020 Visual/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork_2020_Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
